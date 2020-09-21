6b4da8d1dc8840ca055c5ead460c3089ab3c68de
Joe Chipperfield <joechip1709@gmail.com>
2020-09-21 21:36:36 +0100

    Initial commit

---
+++ slider.js
@@ -0,0 +1,8 @@
+const tl = gsap.timeline({defaults: { ease: 'power1.out' } } )
+
+tl.to('.text', { y:'0%', duration: 1.2, stagger: 0.3 });
+tl.to('.slider',{ y: '-100%', duration: 1.5, delay: 1});
+tl.to('.intro', {y: '-100%', duration: 1}, '-=1');
+tl.fromTo('nav', {opacity: 0}, {opacity: 1, duration: 0.5, duration: 0.5});
+tl.fromTo('.hero-content', {opacity: 0}, {opacity: 1, duration: 0.5, duration: 0.5}, '-=0.5');
+tl.fromTo('.scrolldown', {opacity: 0}, {opacity: 1, duration: 0.5, duration: 0.5}, '-=0.5');
