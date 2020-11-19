4741347920ffb3be8f34338a8ef83f8419ce77a9
Joe Chipperfield <joechip1709@gmail.com>
2020-11-19 10:14:54 +0000

    Updates For Speed

--- slider.js
+++ slider.js
@@ -1,7 +1,7 @@
 const tl = gsap.timeline({defaults: { ease: 'power1.out' } } )
 
-tl.to('.text', { y:'0%', duration: 1.2, stagger: 0.3 });
-tl.to('.slider',{ y: '-100%', duration: 1.5, delay: 1});
+tl.to('.text', { y:'0%', duration: 1, stagger: 0.3 });
+tl.to('.slider',{ y: '-100%', duration: 1, delay: 0.8});
 tl.to('.intro', {y: '-100%', duration: 1}, '-=1');
 tl.fromTo('nav', {opacity: 0}, {opacity: 1, duration: 0.5, duration: 0.5});
 tl.fromTo('.hero-content', {opacity: 0}, {opacity: 1, duration: 0.5, duration: 0.5}, '-=0.5');
